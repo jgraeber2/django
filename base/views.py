e4ad7b2900025b3d6f3cbf37af4476305fb59c90
[jgraeber2] <[jgraeber@uncc.edu]>
2025-02-17 19:33:01 -0500

    Create studybud

--- base/views.py
+++ base/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
+rooms = [
+    {'id' :1, 'name' : 'Lets learn python!'},
+    {'id' :2, 'name' : 'Design with me!'},
+    {'id' :3, 'name' : 'Frontend developers'},
+]
+
+
 
 def home(request):
-    return HttpResponse('Home page')
+    context = {'rooms':rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
-    return HttpResponse('ROOM')
+    return render(request, 'room.html')
