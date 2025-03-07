b57168d8863b65aa4018185b2e89563447238593
[jgraeber2] <[jgraeber@uncc.edu]>
2025-03-07 00:13:06 -0500

    Create studybud

--- base/views.py
+++ base/views.py
@@ -76,13 +76,15 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics' : topics, 'room_count':room_count}
+    context = {'rooms': rooms, 'topics' : topics, 
+               'room_count':room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
@@ -97,6 +99,12 @@ def room(request, pk):
     context = {'room': room, 'room_messages':room_messages, 'participants': participants}
 
     return render(request, 'base/room.html', context)
+
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    context = {'user': user}
+    return render(request, 'base/profile.html', context)
 
 
 @login_required(login_url='login')
